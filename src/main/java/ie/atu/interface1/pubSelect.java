b65f44d32aaeb0cb320bb48a9f1492d3068c7094
Aidan Malone <g00423306@atu.ie>
2024-03-30 21:30:54 +0000

    Created rating/ review constructors, string and double, that extend into pubOverview plus details of review system, using arrays

--- src/main/java/ie/atu/interface1/pubSelect.java
+++ src/main/java/ie/atu/interface1/pubSelect.java
@@ -3,16 +3,31 @@ package ie.atu.interface1;
 public class pubSelect {
     private String[] drinks;
     private double[] prices;
+    public String[] reviewCat;
+    public double[] rating;
+
 
     public pubSelect(String[] drinks, double[] prices){
         this.drinks = drinks;
         this.prices = prices;
+
     }
 
-    public void displayinfo(){
+   // public pubSelect(String[] reviewCat, double[] rating){
+    //    this.reviewCat = reviewCat;
+    //    this.rating = rating;
+   // }
+    public void displayinfo() {
         System.out.println("Drink Menu + Prices: ");
-        for(int i = 0; i < drinks.length; i++){
+        for (int i = 0; i <= drinks.length; i++) {
             System.out.println(drinks[i] + ": €" + prices[i]);
         }
     }
+   // public void displayReviewInfo(){
+    //    System.out.println("Ratings and Reviews: ");
+    //    for(int i = 0; i <= reviewCat.length; i++){
+    //        System.out.println(reviewCat[i] + ": " + rating[i] + "/10.");
+      //  }
+   // }
+
 }
